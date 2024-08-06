a1e0558c7b4f253e9bbceb40ff0e8fbdeb00d7c1
Pyriac <cyriacpineau@gmail.com>
2024-08-06 12:19:11 +0200

    creating roads and DB for category and program

--- server/app/controllers/categoryAction.js
+++ server/app/controllers/categoryAction.js
@@ -1,3 +1,5 @@
+const tables = require("../../database/tables");
+
 const categories = [
   {
     id: 1,
@@ -11,8 +13,10 @@ const categories = [
 
 // Declare the actions
 
-const browse = (req, res) => {
-  res.json(categories);
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
